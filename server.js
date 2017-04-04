16af7043f9e15fd942ea05087c1cf585cd0e6403
Christophe DUFOUR <christophedufour@iMac-de-Christophe-DUFOUR-2.local>
2017-04-04 13:45:00 +0200

    Add project page

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs')
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs')
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -45,6 +45,12 @@ app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About page'
   });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  })
 });
 
 app.get('/bad', (req, res) => {
